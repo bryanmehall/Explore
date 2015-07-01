0d0a04b6e1f5d9e8f4b4c054f0d41448ce348ca8
Bryan Mehall <bryanmehall@gmail.com>
2015-06-30 18:21:23 -0600

    asynch
    changing so that createObject is not asynchronous

---
+++ run.js
@@ -0,0 +1,70 @@
+window.onload = function(){
+	var app = window.app
+	var box = document.getElementById('box')
+	
+	var basetemplates = ['T0','T1']
+	basetemplates.forEach(function(item){
+		app.loadTemplate(item,function(){})
+	})
+	
+	var searchTag = function(tag, lib) {
+		var matches = [];
+		for (var i in lib) {
+			if (lib[i].name.indexOf(tag) > -1) {
+				matches.push(lib[i])
+			}
+		}
+		return matches
+	}
+	
+	var newObjectMenu = function() {
+		var box = document.createElement('div');
+		box.style.position = 'absolute'
+		box.style.top = '100px'
+		box.style.backgroundColor = '#fafafa'
+		var textArea = document.createElement('input');
+		box.appendChild(textArea)
+		var options = document.createElement('div')
+		box.appendChild(options)
+		document.body.appendChild(box)
+
+		textArea.addEventListener('keyup', function() {
+			var matches = searchTag(this.value, app.templateCache)
+			if (matches.length === 1) {
+				document.body.removeChild(box)
+				app.createObject(matches[0].templateID,[],function(){})
+			}
+			var newOptions = document.createElement('div');
+			matches.forEach(function(item) {
+				var itemDom = document.createElement('div');
+				itemDom.innerText = item.name;
+				newOptions.appendChild(itemDom)
+			})
+			box.replaceChild(newOptions, options)
+			options = newOptions
+		})
+		textArea.focus()
+	}
+	
+	document.addEventListener('keypress',function(event){
+		var key = String.fromCharCode(event.keyCode);
+		if (key ==="\\"){
+			newObjectMenu()
+		}
+	})
+	
+	
+	box.addEventListener('click',function(){
+		box.focus()
+	})
+	box.addEventListener('keyup',function(event){
+		app.createObject('T1',function(integer){
+		integer.properties.value.set(String.fromCharCode(event.keyCode))
+	});
+	
+	
+	})
+	
+
+	
+}
